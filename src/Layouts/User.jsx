56af048f7373cba103131eef7d8095c4451fedb1
rajat-das247365 <rd540032@gmail.com>
2026-01-16 21:31:21 +0530

    a

--- src/Layouts/User.jsx
+++ src/Layouts/User.jsx
@@ -1,9 +1,124 @@
 import React from 'react'
+import UserTableData from './UserTableData'
+import { FaPen } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
 
+
+
+
+const headings = [
+  {
+    user : "Vivek Jha",
+    role : "Admin",
+    status : "Active",
+      actions : <FaPen />,
+    actions2 : <MdDelete/>
+    
+
+
+  }
+
+]
+const headings1 = [
+  {
+    user : "Michael Mishra",
+    role : "Editor",
+    status : "Inactive",
+      actions : <FaPen />,
+    actions2 : <MdDelete/>
+    
+
+
+  }
+
+]
+const headings2 = [
+  {
+    user : "Payal Sen",
+    role : "Admin",
+    status : "Pending",
+      actions : <FaPen />,
+    actions2 : <MdDelete/>
+    
+
+
+  }
+
+]
+const headings3 = [
+  {
+    user : "Sam Sharma",
+    role : "Viewer",
+    status : "Inactive",
+      actions : <FaPen />,
+    actions2 : <MdDelete/>
+    
+
+
+  }
+
+]
+const headings4 = [
+  {
+    user : "Casey Roy",
+    role : "Admin",
+    status : "Inactive",
+      actions : <FaPen />,
+    actions2 : <MdDelete/>
+    
+
+
+  }
+
+]
+const headings5 = [
+  {
+    user : "Nandi Maharaj",
+    role : "Viewer",
+    status : "Active",
+      actions : <FaPen  />,
+    actions2 : <MdDelete/>
+    
+
+
+  }
+
+]
+const headings6 = [
+  {
+    user : "Dora Saru",
+    role : "Editor",
+    status : "Pending",
+      actions : <FaPen />,
+    actions2 : <MdDelete />
+    
+
+
+  }
+
+]
 const User = () => {
   return (
-    <div>
-      jdhjdhdh
+    <div className='w-full  bg-[#0D1117] flex justify-center mt-24 text-slate-200 '>
+      <div className='table   w-6xl bg-[#161B22]  rounded-lg  border border-slate-700 '>
+       <table className='w-full  text-left  '>
+        <thead className='h-14 border border-b-slate-700 border-t-0 border-l-0 border-r-0'>
+        <tr className='p-2'>
+          <th className='px-6'>User</th>
+          <th className='px-6'>Role</th>
+          <th className='px-6'>Status</th>
+          <th className='px-6'>Actions</th>
+        </tr>
+        </thead>
+        <UserTableData headings= {headings}/>
+        <UserTableData headings= {headings1}/>
+        <UserTableData headings= {headings2}/>
+        <UserTableData headings= {headings3}/>
+        <UserTableData headings= {headings4}/>
+        <UserTableData headings= {headings5}/>
+        <UserTableData headings= {headings6}/>
+       </table>
+      </div>
     </div>
   )
 }
---
+++ src/Layouts/UserTableData.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const UserTableData = ({headings}) => {
+  return (
+    <tbody>
+        {headings.map((item,id)=>
+      <tr key={id} className=' h-14 border border-b-slate-700 border-t-0 border-l-0 border-r-0 hover:bg-slate-700/50'>
+        <td className='px-6 text-xl'>{item.user}</td>
+        <td className='px-6 text-xl'>{item.role}</td>
+        <td className='px-6 text-xl'>{item.status}</td>
+        <td className='px-6 text-xl text-indigo-600'>{item.actions}</td>
+        <td className='px-6 text-xl text-red-600'>{item.actions2}</td>
+      </tr>
+      )}
+    </tbody> 
+  )
+}
+
+export default UserTableData
